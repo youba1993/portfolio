3a2168c0837fc119443fd6c8d73323e432e994a3
youbaidjis <youba.etud@gmail.com>
2022-10-19 18:04:48 -0700

    Updated

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -67,10 +67,8 @@ export default function Contact() {
                 </div>
                 <form
                     onSubmit={() => handleSubmit()}
-                    netlify
                     name="contact"
                     className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
-                    <input type="hidden" name="contact" value="contact" />
                     <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
                         Hire Me
                     </h2>
